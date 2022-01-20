b12e44c4a06c82448c23d01bf85a8c6cf4679a88
XiaoFeiDu <romanchaos@163.com>
2022-01-20 16:53:01 +0800

    recode

--- demos/CodeTimerDemo/Program.cs
+++ demos/CodeTimerDemo/Program.cs
@@ -6,17 +6,12 @@ Console.WriteLine("Begin!");
 
 var bytes = new byte[1000000000];
 
-var summaries = new List<Summary>();
-
 CodeTimer.Initialize();
 
-summaries.Add(CodeTimer.Time("Length",1000, () => { var result = bytes.Length; }));
-summaries.Add(CodeTimer.Time("LongLength",1000, () => { var result = bytes.LongLength; }));
-summaries.Add(CodeTimer.Time("NotAny",1000, () => { var result = !bytes.Any(); }));
+Console.WriteLine(CodeTimer.Time("Length",1000, () => { var result = bytes.Length; }));
+Console.WriteLine(CodeTimer.Time("LongLength",1000, () => { var result = bytes.LongLength; }));
+Console.WriteLine(CodeTimer.Time("NotAny",1000, () => { var result = !bytes.Any(); }));
 
-foreach (var summary in summaries)
-    Console.WriteLine(summary);
-
-Console.WriteLine($"{summaries.Count} Complete!");
+Console.WriteLine("Complete!");
 
 Console.ReadLine();
